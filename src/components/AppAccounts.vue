38fb6066c242bb591774851f217e7acd8d915197
lochhonest <116290675+lochhonest@users.noreply.github.com>
2022-12-19 22:15:43 +0100

    Update AppAccounts.vue

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -293,7 +293,7 @@
 
           //GET Recipes
           RESTgetRecipes() {
-          const path = `http://127.0.0.1:5000/recipes`;
+          const path = `${process.env.VUE_APP_ROOT_URL}/recipes`;
           axios
               .get(path)
               .then((response) => {
@@ -314,7 +314,7 @@
 
           //POST Recipes
           RESTcreateRecipe(payload){
-              const path = `http://127.0.0.1:5000/recipe`;
+              const path = `${process.env.VUE_APP_ROOT_URL}/recipe`;
               axios
                   .post(path,payload)
                   .then((response) => {
@@ -334,7 +334,7 @@
           },
 
           RESTupdateRecipe(payload, id){
-              const path = `http://127.0.0.1:5000/recipe/${id}`;
+              const path = `${process.env.VUE_APP_ROOT_URL}/recipe/${id}`;
               axios
                   .put(path,payload)
                   .then((response) => {
@@ -355,7 +355,7 @@
 
 
           RESTdeleteRecipe(id){
-              const path = `http://127.0.0.1:5000/recipe/${id}`;
+              const path = `${process.env.VUE_APP_ROOT_URL}/recipe/${id}`;
               axios
                   .delete(path)
                   .then((response) => {
